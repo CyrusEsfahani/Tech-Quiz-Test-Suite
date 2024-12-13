ab194d5bdd92c83909073976b370e763533fc6e3
Heisenberg <esfahani.cyrus@gmail.com>
2024-12-13 07:42:09 -0800

    added alot of updates

--- cypress/e2e/quiz.cy.js
+++ cypress/e2e/quiz.cy.js
@@ -1,24 +1,27 @@
-describe('Tech Quiz', () => {
-    it('should start the quiz and present a question', () => {
+describe('Quiz component', () => {
+    beforeEach(() => {
       cy.visit('/');
-      cy.get('button').contains('Start').click();
-      cy.get('.question').should('exist');
     });
   
-    it('should present another question after answering', () => {
-      cy.get('.answer').first().click();
-      cy.get('.question').should('exist');
+    it('should start the quiz and display the first question', () => {
+        cy.get('button').contains('Start Quiz').click();
+        cy.get('.card').should('be.visible');
+        cy.get('h2').should('not.be.empty');
     });
-  
-    it('should show the score after all questions are answered', () => {
-      for (let i = 0; i < 10; i++) {
-        cy.get('.answer').first().click();
-      }
-      cy.get('.score').should('exist');
+    it('should answer questions and complete the quiz', () => {
+        cy.get('button').contains('Start Quiz').click();
+        for (let i = 0; i < 10; i++) {
+            cy.get('button').contains('1').click();
+        }
+        cy.get('.alert-success').should('be.visible').and('contain', 'Your score');
     });
-  
-    it('should allow starting a new quiz after finishing', () => {
-      cy.get('button').contains('Start New Quiz').click();
-      cy.get('.question').should('exist');
+    it('should restart the quiz after completion', () => {
+        cy.get('button').contains('Start Quiz').click();
+        for (let i = 0; i < 10; i++) {
+            cy.get('button').contains('1').click();
+        }
+        cy.get('button').contains('Take New Quiz').click();
+        cy.get('.card').should('be.visible');
+        cy.get('h2').should('not.be.empty');
     });
-  });
+});
